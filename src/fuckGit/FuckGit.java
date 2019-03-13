85f6feb00a256be63daaac533a9dbf6d8cd5f050
Administrator <Administrator@192.168.0.107>
2019-03-13 17:25:02 +0800

    test

---
+++ src/fuckGit/FuckGit.java
@@ -0,0 +1,12 @@
+package fuckGit;
+
+public class FuckGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<10;i++){
+			System.out.println("fuck you");
+		}
+	}
+
+}
